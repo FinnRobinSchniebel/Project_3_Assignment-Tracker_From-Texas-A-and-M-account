b18012c7016859c3f8e13e6fedee05768b452828
FRMS <frms@tamu.edu>
2022-04-05 21:09:04 -0500

    Sort is working for demo (for now) - needs to be reconfigured to work with dynamic items -Still needs the date fix for due date sort

--- scriptHomeSort.js
+++ scriptHomeSort.js
@@ -2,13 +2,34 @@ src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
 
 function sortAssignmentByTimeLeft(){
     $('.Assignment').sort(function(a, b){
-        var contentA = parseInt( $(a).data('timeLeft'));
+        var contentA = parseInt( $(a).data('timeleft'));
         
-        var contentB = parseInt( $(b).data('timeLeft'));
+        var contentB = parseInt( $(b).data('timeleft'));
         console.debug(contentA);
+        console.debug(contentB);
         return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
     
     }).appendTo($('.Assignments'));
 }
-
-
+function sortAssignmentByPriority(){
+    $('.Assignment').sort(function(a, b){
+        var contentA = parseInt( $(a).data('priority'));
+        
+        var contentB = parseInt( $(b).data('priority'));
+        console.debug(contentA);
+        console.debug(contentB);
+        return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
+    
+    }).appendTo($('.Assignments'));
+}
+function sortAssignmentByDueDate(){
+    $('.Assignment').sort(function(a, b){
+        var contentA = parseInt( $(a).data('due'));
+        
+        var contentB = parseInt( $(b).data('due'));
+        console.debug(contentA);
+        console.debug(contentB);
+        return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
+    
+    }).appendTo($('.Assignments'));
+}
