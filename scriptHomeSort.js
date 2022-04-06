e805d061d998d0816d97c20a467cbca296db1709
FRMS <frms@tamu.edu>
2022-04-06 16:32:10 -0500

    Working on sort but it isnt finished. - will complete in next sprint when the storage is done for easier use.

--- scriptHomeSort.js
+++ scriptHomeSort.js
@@ -1,7 +1,9 @@
 src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
 
-function sortAssignmentByTimeLeft(location){
-    location.find(('.Assignment')).sort(function(a, b){
+function sortAssignmentByTimeLeft(locationD){
+    console.debug(locationD);
+    
+    locationD.find('.Assignment').sort(function(a, b){
         var contentA = parseInt( $(a).data('timeleft'));
         
         var contentB = parseInt( $(b).data('timeleft'));
@@ -9,10 +11,14 @@ function sortAssignmentByTimeLeft(location){
         console.debug(contentB);
         return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
     
-    }).appendTo(location);
+    }).appendTo(locationD);
 }
+
+
+
 function sortAssignmentByPriority(location){
-    location.find(('.Assignment')).sort(function(a, b){
+    var divs =location.getElementsByClassName(('.Assignment'));
+    location.find('.Assignment').sort(function(a, b){
         var contentA = parseInt( $(a).data('priority'));
         
         var contentB = parseInt( $(b).data('priority'));
@@ -23,7 +29,7 @@ function sortAssignmentByPriority(location){
     }).appendTo(location);
 }
 function sortAssignmentByDueDate(location){
-    location.find(('.Assignment')).sort(function(a, b){
+    location.getElementsByClassName(('Assignment')).sort(function(a, b){
         var contentA = parseInt( $(a).data('due'));
         
         var contentB = parseInt( $(b).data('due'));
