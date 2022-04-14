442fb2b47fd523981cd91d2bff0fcfbd76e7e5d5
FRMS <frms@tamu.edu>
2022-04-13 20:53:45 -0500

    Sort by due date and priority fully functional

--- SortScript.js
+++ SortScript.js
@@ -4,6 +4,7 @@ var CurSort = "DueDate";
 
 function sortButton(sortby){
     CurSort = sortby;
+    console.log(CurSort);
     populatePage();
 }   
 
@@ -12,23 +13,26 @@ function sortButton(sortby){
 //called by populating function for sorting the array
 function sortAssignment(JSONOBJ){
 
-    if(CurSort == "Duedate"){
+    if(CurSort == "DueDate"){
         //order primarily by due date but first by priority so if something has the same duedate the one with the higher priority will go first
-        console.log("here");
         JSONOBJ = sortPrior(JSONOBJ);
         JSONOBJ = sortDue(JSONOBJ); 
     }
     else if(CurSort == "Priority"){
-        console.log("here");
         JSONOBJ = sortDue(JSONOBJ);
         JSONOBJ = sortPrior(JSONOBJ);
+    }
+    else{
+        console.log("error");
     }
 
     return JSONOBJ;
 }
 function sortDue(OBJ){
     return OBJ.sort( function(x,y){
-        return Date(x.dueDate).getTime() - Date(y.dueDate).getTime();
+        console.log("2022-04-15T18:22" < "2022-04-29T18:23");
+        //using ISO date sort the extra stuff is for correctness. For source: https://stackoverflow.com/questions/12192491/sort-array-by-iso-8601-date 
+        return ((x.dueDate < y.dueDate) ? -1 : ((x.dueDate > y.dueDate)) ? 1 : 0); 
     });
 }
 function sortPrior(OBJ){
