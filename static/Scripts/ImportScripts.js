e9c392914fb94fc804a0e24ae3dd8fdbfb09ded7
fapadron732 <fapadron732@tamu.edu>
2022-04-16 14:39:52 -0500

    Add files via upload

---
+++ static/Scripts/ImportScripts.js
@@ -0,0 +1,42 @@
+
+function CT(Option){
+    $('#ImportInfo').collapse();
+    if(Option == "none"){
+        $('#ImportInfo').collapse("hide");
+    }
+    else if(Option == "Canvas"){
+        clearImportCont(); //clear any other content
+
+        addinfoGet("#CanvasInfoTemp");
+        var check;
+        $('#ImportInfo').collapse("show");
+    }
+    else if(Option == "Google"){
+        clearImportCont();
+
+        addinfoGet("#ClassRoomTemp");
+        $('#ImportInfo').collapse("show");
+    }
+    
+}
+function clearImportCont(){
+    document.getElementById("CollapseCont").innerHTML ="";
+}
+
+
+function addinfoGet(fromTemp){
+    var NewHTML = document.querySelector(fromTemp).content;
+    NewHTML= NewHTML.cloneNode(true);
+    document.getElementById("CollapseCont").append(NewHTML);
+}
+
+
+function  ImportAPICanvas(){
+    document.getElementById('SelectLocation').innerHTML += `<div class="loader"></div>`;
+
+    //function for api call here (return file or bool)
+
+    //populate options here (with json results) 
+
+    
+}
