799093f110048ed97ea8557cab97f3e5a425d4f0
FRMS <frms@tamu.edu>
2022-04-15 21:08:15 -0500

    Created a Half functioning dropdown for info

--- Scripts/ImportScripts.js
+++ Scripts/ImportScripts.js
@@ -5,10 +5,33 @@ function CT(Option){
         $('#ImportInfo').collapse("hide");
     }
     else if(Option == "Canvas"){
+        clearImportCont();
+        addinfoGetCanvas();
+        var check;
+        setTimeout(check, 300);
         $('#ImportInfo').collapse("show");
     }
     else if(Option == "Google"){
+        clearImportCont();
         $('#ImportInfo').collapse("show");
     }
+    
+}
+function clearImportCont(){
+    document.getElementById("CollapseCont").innerHTML ="";
+}
+
+
+function addinfoGetCanvas(){
+    var NewHTML = document.querySelector("#CanvasInfoTemp").content;
+
+
+    NewHTML= NewHTML.cloneNode(true);
+
+    document.getElementById("CollapseCont").append(NewHTML);
+}
+
+
+function  ImportAPICanvas(){
     
 }
