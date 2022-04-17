e38660279e8b878554c0a29bab5f2d53f3c2a748
FRMS <frms@tamu.edu>
2022-04-17 14:18:37 -0500

    Basic code setup for Import populate

--- Scripts/ImportScripts.js
+++ Scripts/ImportScripts.js
@@ -40,3 +40,41 @@ function  ImportAPICanvas(){
 
     
 }
+
+
+function assignmentPop(ClassList){
+    var listlen = ClassList.length;
+
+    var ClassList = getClassList();
+
+    var optionObj = document.createElement('div'); //will hold the options
+
+    //add options for dropdown to temperary div
+    for(var i =0; i < ClassList.length; i++){
+        optionObj.innerHTML += `<option value="`+ ClassList[i].name +`" selected> `+ ClassList[i].name +`</option>`;
+    }
+
+    for(var i =0; i < listlen; i++){
+        var NewHTML = document.querySelector(fromTemp).content;
+        NewHTML= NewHTML.cloneNode(true);
+        //document.getElementById("CollapseCont").append(NewHTML);
+
+
+        //note: this works because there is only one of each
+        var cur = NewHTML.querySelectorAll('div')[0]; //div
+        cur.setAttribute('id', '' + cur.id +i);
+        
+        var cur = NewHTML.querySelectorAll('select')[0]; //select
+        cur.setAttribute('id', '' + cur.id +i);
+
+        var cur = NewHTML.querySelectorAll('label')[0]; //label
+        cur.setAttribute('id', ''+ cur.id +i);
+        cur.setAttribute('for', ''+ cur.for +i);
+
+        //add all valid options
+        document.getElementById('NewLoc'+ i).innerHTML += optionObj.innerHTML;
+
+       document.getElementById('SelectLocation').innerHTML += NewHTML;
+    }
+    return; //returns nothing atm
+}
