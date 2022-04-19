1b372c41a0d7aa7069e0b3b59b302c4603fdb149
fapadron732 <fapadron732@tamu.edu>
2022-04-19 12:17:07 -0500

    added function outlines for importing from canvas

--- app.py
+++ app.py
@@ -161,14 +161,72 @@ def getGoogleJSONs():
 
 #background process happening without any refreshing
 @app.route('/bgGetCanvasUser', methods=['GET', 'POST'])
-def getCanvasUser():
+def getCanvasUser(token):
     url = "https://canvas.tamu.edu/api/v1/users/self"
 
-    headers = {'Authorization' : 'Bearer 15924~j9hFN1TVjJHgUCE7CyTdrKaWKSLsf8yIRaCtCfQlXp4PkASi8ts3UJEqn2ackRq1'}
+    #eventually will take bearer token as an argument
+    token = "15924~j9hFN1TVjJHgUCE7CyTdrKaWKSLsf8yIRaCtCfQlXp4PkASi8ts3UJEqn2ackRq1"
+    headers = {'Authorization' : 'Bearer '+ token}
 
     resp = requests.get(url, headers=headers)
 
     return (resp.json())
+
+
+#will call for all courses of a user
+@app.route('/bgGetCanvasCourses', methods=['GET', 'POST'])
+def getCanvasCourses(token, userID):
+    url = "https://canvas.tamu.edu/api/v1/users/"+userID+"/courses"
+
+    #eventually will take bearer token as an argument
+    token = "15924~j9hFN1TVjJHgUCE7CyTdrKaWKSLsf8yIRaCtCfQlXp4PkASi8ts3UJEqn2ackRq1"
+    headers = {'Authorization' : 'Bearer '+ token}
+
+    resp = requests.get(url, headers=headers)
+
+    return (resp.json())
+
+
+#function will call for assignments in a course
+@app.route('/bgGetCanvasAssignments', methods=['GET', 'POST'])
+def getCanvasAssignments(token, courseID):
+    url = "https://canvas.tamu.edu/api/v1/courses/"+courseID+"/assignments"
+
+    #eventually will take bearer token as an argument
+    token = "15924~j9hFN1TVjJHgUCE7CyTdrKaWKSLsf8yIRaCtCfQlXp4PkASi8ts3UJEqn2ackRq1"
+    headers = {'Authorization' : 'Bearer '+ token}
+
+    resp = requests.get(url, headers=headers)
+
+    return (resp.json())
+
+#this will be the main function for getting all classes and assignments from a user
+@app.route('/bgCanvasImport', methods=['GET', 'POST'])
+def getCanvasJSONs(token):
+    ## TO DO
+    
+    ## This function needs to take in the token entered by user
+
+    ## For testing purposes 
+    token = "15924~j9hFN1TVjJHgUCE7CyTdrKaWKSLsf8yIRaCtCfQlXp4PkASi8ts3UJEqn2ackRq1"
+
+    #get canvas userID using this function
+    #userID = getCanvasUser(token)
+
+    #get an array of classIDs using this function
+    #courseIDs = getCanvasCourses(token, userID)
+
+    #iterate through classID list and get all assignments for each course
+    #for each course: assignmentList = getCanvasAssignments(token, courseID)
+        #create dictionarys and structure just like local JSON objects (classObjs & assignmentObjs)
+
+    
+    #return an array of dictionarys (classObjs)
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
